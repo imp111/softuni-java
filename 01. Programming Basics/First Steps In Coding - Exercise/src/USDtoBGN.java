07b947501c6795489c0956959223a69511de508f
imp111 <alexsandartenev@gmail.com>
2023-05-03 09:47:46 +0300

    usdToBgn added

---
+++ 01. Programming Basics/First Steps In Coding - Exercise/src/USDtoBGN.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class USDtoBGN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double dollars = scanner.nextDouble() * 1.79549;
+
+        System.out.println(dollars);
+    }
+}
