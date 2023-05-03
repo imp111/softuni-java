a11a87952ffb09bdf4e7f528e83e91244e994a55
imp111 <alexsandartenev@gmail.com>
2023-05-03 09:27:20 +0300

    project creating done

--- 01. Programming Basics/01. First Steps In Coding - Lab/First Steps In Coding - Lab/src/ProjectsCreation.java
+++ 01. Programming Basics/01. First Steps In Coding - Lab/First Steps In Coding - Lab/src/ProjectsCreation.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class ProjectsCreation {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String architectName = scanner.nextLine();
+        int numberOfProjects = Integer.parseInt(scanner.nextLine());
+        int timePerProject = 3;
+        int hours = numberOfProjects * timePerProject;
 
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.", architectName, hours, numberOfProjects);
     }
 }
