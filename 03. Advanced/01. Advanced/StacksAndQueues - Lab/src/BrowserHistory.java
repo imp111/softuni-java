a824d74c15f2aaa960b19981d90808f21d3710bd
imp111 <alexsandartenev@gmail.com>
2023-10-06 23:47:32 +0300

    01. Sum Seconds - added and done (100/100)

--- 03. Advanced/01. Advanced/StacksAndQueues - Lab/src/BrowserHistory.java
+++ 03. Advanced/01. Advanced/StacksAndQueues - Lab/src/BrowserHistory.java
@@ -1,5 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class BrowserHistory {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        ArrayDeque<String> stack = new ArrayDeque<String>();
+        String current = "";
+
+        while (!input.equalsIgnoreCase("home")) {
+            if (input.equalsIgnoreCase("back")) {
+                if (stack.size() <= 1) {
+                    System.out.println("no previous URLs");
+                }
+                else {
+                    current = stack.peek();
+                    System.out.println(current);
+                }
+            }
+            else {
+                stack.push(input);
+                current = stack.peek();
+                System.out.println(current);
+            }
+
+            input = scanner.nextLine();
+        }
     }
 }
