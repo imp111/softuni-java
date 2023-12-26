1de8f8404c6d3dac2e939b1c1ab5841316ebe1b2
imp111 <alexsandartenev@gmail.com>
2023-12-26 17:56:47 +0200

    04. First and Reserve Team - added and done (77/100, because of access modifiers of the firstName, lastName and age setters )

---
+++ 03. Advanced/02. OOP/Encapsulation - Lab/src/FirstAndReserveTeam/Person.java
@@ -0,0 +1,80 @@
+package FirstAndReserveTeam;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        if (firstName.length() < 3) {
+            throw new IllegalArgumentException("Names must be at least 3 symbols");
+        }
+
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    private void setLastName(String lastName) {
+        if (lastName.length() < 3) {
+            throw new IllegalArgumentException("Names must be at least 3 symbols");
+        }
+
+        this.lastName = lastName;
+    }
+
+    private void setAge(int age) {
+        if (age <= 0) {
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+
+        this.age = age;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    private void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary can't less than 460 leva");
+        }
+
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void increaseSalary (double percentage) {
+        if (this.getAge() < 30) {
+            this.setSalary((this.getSalary() + (this.getSalary() * percentage / 200)));
+        }
+        else {
+            this.setSalary(this.getSalary() + (this.getSalary() * percentage / 100));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %f leva.",
+                this.getFirstName(),
+                this.getLastName(),
+                this.getSalary());
+    }
+}
--- 03. Advanced/02. OOP/Encapsulation - Lab/src/ValidationData/Main.java
+++ 03. Advanced/02. OOP/Encapsulation - Lab/src/ValidationData/Main.java
@@ -1,5 +1,4 @@
-import FirstAndReserveTeam.Team;
-import FirstAndReserveTeam.Person;
+package ValidationData;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,24 +10,22 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        List<Person> players = new ArrayList<>();
-        Team team = new Team("Black eagles");
+        List<Person> people = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             String[] input = reader.readLine().split(" ");
             try {
-                players.add(new Person(input[0], input[1], Integer.parseInt(input[2]),
+                people.add(new Person(input[0], input[1], Integer.parseInt(input[2]),
                         Double.parseDouble(input[3])));
             } catch (Exception ex) {
                 System.out.println(ex.getMessage());
             }
         }
 
-        for (Person player : players) {
-            team.addPlayer(player);
+        double bonus = Double.parseDouble(reader.readLine());
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+            System.out.println(person.toString());
         }
-
-        System.out.println("First team have " + team.getFirstTeam().size() + " players");
-        System.out.println("First team have " + team.getReserveTeam().size() + " players");
     }
 }
