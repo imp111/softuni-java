573ef1466f3b9da861989ba729fbf3de1889dd2a
imp111 <alexsandartenev@gmail.com>
2024-04-17 06:33:12 +0300

    03. Say Hello Extended - added and done

---
+++ 03. Advanced/02. OOP/Abstraction and Interfaces - Lab/src/SayHelloExtended/Main.java
@@ -0,0 +1,21 @@
+package SayHelloExtended;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<>();
+
+        persons.add(new Bulgarian("Peter"));
+        persons.add(new European("Gosho"));
+        persons.add(new Chinese("Donio"));
+        for (Person person : persons) {
+            print(person);
+        }
+    }
+
+    private static void print(Person person) {
+        System.out.println(person.sayHello());
+    }
+}
